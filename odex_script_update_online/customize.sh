52713edd9752c859ec5010f03545229779a18d6f
柚稚的孩纸 <30484319+zjw2017@users.noreply.github.com>
2023-08-01 22:27:43 +0800

    更新 customize.sh

--- odex_script_update_online/customize.sh
+++ odex_script_update_online/customize.sh
@@ -63,4 +63,4 @@ EOF
 [ -f /storage/emulated/0/Android/MIUI_odex/odex.json ] && rm -rf /storage/emulated/0/Android/MIUI_odex/odex.json
 set_perm_recursive "$MODPATH" 0 0 0755 0644
 set_perm_recursive "$MODPATH"/system/bin 0 2000 0755 0755
-ui_print "- 安装完成,请重启后运行odex.sh"
+ui_print "- 安装完成,请重启后运行/storage/emulated/0/Android/MIUI_odex/odex.sh"
